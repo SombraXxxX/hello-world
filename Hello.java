c5ffcc1a308fc9b92d4b08a9ef77f2b4be9720d2
SombraXxxX <43217593+SombraXxxX@users.noreply.github.com>
2018-09-12 14:44:56 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Computer Science!");
+        System.out.println("Hello, AP Computer Science!");
     }
 }
